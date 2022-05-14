d4386c8918f8ae3206d3cf7f7997d713c7b1c3f8
mario pratama <mario.pratama@xtremax.com>
2022-05-14 12:31:21 +0700

    Add login feature

---
+++ login.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace  Belajar.Git
+{
+    class login
+    {
+        static void Main (string [] args)
+        {
+            //ini untuk menjalankan aplikasi
+        }
+    }
+}
